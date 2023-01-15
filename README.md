da78680eec92185bb968da6467da9de377affa79
dev Kim <windofwind@users.noreply.github.com>
2023-01-15 17:09:32 +0900

    💄 apply tailwind apply dto apply lint, formatter

--- README.md
+++ README.md
@@ -4,10 +4,14 @@ nextjs 샘플 프로젝트 입니다. cloudflre로 배포합니다.
 
 목적은 nextjs 의 api 라우팅 부분 체크 목적입니다.
 
+# 테스트 설치 모듈
+```bash
+
+```
 ## nextjs 기본
 
 - [x] nextjs 는 root에 pages폴더가 존재하거나 src 폴더 안에 존재해야합니다.
-- [x] next,cinfig.js 에 swc 사용함
+- [x] next,config.js 에 swc 사용함
 - [ ] primeface 모듈 설치하기
 - [ ] api 모듈 확인 하기
 - [ ] cloudflare d1 사용해보기
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import "../common/usePrimeFace.ts";
+import 'reflect-metadata';
+import '../styles/globals.css';
+import '../common/usePrimeFace.ts';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
